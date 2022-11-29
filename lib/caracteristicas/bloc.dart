ade38b7caf083a4bcbfa25a15b6b183040358c93
leyonel <leonelleyonel@gmail.com>
2022-11-29 00:58:43 -0700

    estados y eventos

--- lib/caracteristicas/bloc.dart
+++ lib/caracteristicas/bloc.dart
@@ -4,11 +4,35 @@ class EventoHP {}
 
 class IrAInicio extends EventoHP {}
 
+class IrATodosLosPersonajes extends EventoHP {}
+
+class IrAHechizos extends EventoHP {}
+
+class IrAEstudiantes extends EventoHP {
+  final String casa;
+
+  IrAEstudiantes({required this.casa});
+}
+
+class IrAProfesores extends EventoHP {
+  final String casa;
+
+  IrAProfesores({required this.casa});
+}
+
 class EstadoHP {}
 
 class Inicial extends EstadoHP {}
 
 class PaginaPrincipal extends EstadoHP {}
+
+class VerPersonajes extends EstadoHP {}
+
+class VerHechizos extends EstadoHP {}
+
+class VerEstudiantes extends EstadoHP {}
+
+class VerProfesores extends EstadoHP {}
 
 class HPBloc extends Bloc<EventoHP, EstadoHP> {
   HPBloc() : super(Inicial()) {
