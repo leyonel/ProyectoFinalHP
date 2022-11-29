dc77df35e3f4836217bb9b06994c028f14d9a098
leyonel <leonelleyonel@gmail.com>
2022-11-29 01:03:14 -0700

    partes del dominio

---
+++ lib/dominio/hechizo.dart
@@ -0,0 +1,6 @@
+class Hechizo {
+  final String nombre;
+  final String descripcion;
+
+  Hechizo._(this.nombre, this.descripcion);
+}
---
+++ lib/dominio/personaje.dart
@@ -0,0 +1,42 @@
+class Personaje {
+  final String nombre;
+  final Map nombreA;
+  final String especie;
+  final String genero;
+  final String casa;
+  final String fechaDeNacimiento;
+  final int anoNacimiento;
+  final bool esMago;
+  final String linaje;
+  final String colorDeOjos;
+  final String colorDeCabello;
+  final Map varita;
+  final String patronus;
+  final bool esEstudiante;
+  final bool esStaff;
+  final String actor;
+  final Map actoresAlternativos;
+  final bool estaVivo;
+  final String imagen;
+
+  Personaje._(
+      this.nombre,
+      this.nombreA,
+      this.especie,
+      this.genero,
+      this.casa,
+      this.fechaDeNacimiento,
+      this.anoNacimiento,
+      this.esMago,
+      this.linaje,
+      this.colorDeOjos,
+      this.colorDeCabello,
+      this.varita,
+      this.patronus,
+      this.esEstudiante,
+      this.esStaff,
+      this.actor,
+      this.actoresAlternativos,
+      this.estaVivo,
+      this.imagen);
+}
