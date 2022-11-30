c6c0ab1ba0f743c6f0da7c0dfe8ebecf1b55ad2c
leyonel <leonelleyonel@gmail.com>
2022-11-29 19:44:33 -0700

    obtener estudiantes y staff por casa

--- lib/caracteristicas/verificacion/repositorio_personajes.dart
+++ lib/caracteristicas/verificacion/repositorio_personajes.dart
@@ -16,15 +16,75 @@ class RepositorioPersonajesOffline extends RepositorioPersonajes {
   @override
   Either<Problema, List<Personaje>> obtenerEstudiantesDeCasa(
       String documento, String casa) {
-    // TODO: implement obtenerEstudiantesDeCasa
-    throw UnimplementedError();
+    try {
+      JsonDecoder decoder = const JsonDecoder();
+      final jsonPersonajes = decoder.convert(documento);
+      List<Personaje> personajes = [];
+      for (var personaje in jsonPersonajes) {
+        if (personaje["casa"] == casa && personaje["hogwartsStudent"] == true) {
+          personajes.add(Personaje.constructor(
+              nombre: personaje["name"],
+              nombreA: personaje["alternate_names"],
+              especie: personaje["species"],
+              genero: personaje["gender"],
+              casa: personaje["house"],
+              fechaDeNacimiento: personaje["dateOfBirth"],
+              anoNacimiento: personaje["yearOfBirth"],
+              esMago: personaje["wizard"],
+              linaje: personaje["ancestry"],
+              colorDeOjos: personaje["eyeColour"],
+              colorDeCabello: personaje["hairColour"],
+              varita: personaje["wand"],
+              patronus: personaje["patronus"],
+              esEstudiante: personaje["hogwartsStudent"],
+              esStaff: personaje["hogwartsStaff"],
+              actor: personaje["actor"],
+              actoresA: personaje["alternate_actors"],
+              estaVivo: personaje["alive"],
+              imagen: personaje["image"]));
+        }
+      }
+      return Right(personajes);
+    } catch (e) {
+      return Left(JsonDesactualizado());
+    }
   }
 
   @override
   Either<Problema, List<Personaje>> obtenerStaffDeCasa(
       String documento, String casa) {
-    // TODO: implement obtenerStaffDeCasa
-    throw UnimplementedError();
+    try {
+      JsonDecoder decoder = const JsonDecoder();
+      final jsonPersonajes = decoder.convert(documento);
+      List<Personaje> personajes = [];
+      for (var personaje in jsonPersonajes) {
+        if (personaje["casa"] == casa && personaje["hogwartsStaff"] == true) {
+          personajes.add(Personaje.constructor(
+              nombre: personaje["name"],
+              nombreA: personaje["alternate_names"],
+              especie: personaje["species"],
+              genero: personaje["gender"],
+              casa: personaje["house"],
+              fechaDeNacimiento: personaje["dateOfBirth"],
+              anoNacimiento: personaje["yearOfBirth"],
+              esMago: personaje["wizard"],
+              linaje: personaje["ancestry"],
+              colorDeOjos: personaje["eyeColour"],
+              colorDeCabello: personaje["hairColour"],
+              varita: personaje["wand"],
+              patronus: personaje["patronus"],
+              esEstudiante: personaje["hogwartsStudent"],
+              esStaff: personaje["hogwartsStaff"],
+              actor: personaje["actor"],
+              actoresA: personaje["alternate_actors"],
+              estaVivo: personaje["alive"],
+              imagen: personaje["image"]));
+        }
+      }
+      return Right(personajes);
+    } catch (e) {
+      return Left(JsonDesactualizado());
+    }
   }
 
   @override
